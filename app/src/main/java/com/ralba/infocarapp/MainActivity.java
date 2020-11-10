55c2ae0ee5fa6f2f531e846e60b2a9526410ef46
Rafa-AG <ralbaglez@gmail.com>
2020-11-10 14:05:59 +0100

    Prueba

--- app/src/main/java/com/ralba/infocarapp/MainActivity.java
+++ app/src/main/java/com/ralba/infocarapp/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //hola
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
